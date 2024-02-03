8dd99eff69f627803c947237e68d20033ab7260b
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-02-03 20:06:56 +0100

    hs

--- valentine_try.py
+++ valentine_try.py
@@ -4,7 +4,6 @@ import json
 from valentine import valentine_match
 from valentine.algorithms import Coma
 
-
 dataSource = "sources-json"
 mappingsSource = "mappings"
 files = os.listdir(dataSource)
@@ -22,15 +21,19 @@ totalSchemas = len(schemaList)
 totalComparisons = totalSchemas * (totalSchemas-1) / 2
 print(f"total schemas: {totalSchemas}")
 print(f"total comparisons to do: {totalComparisons}")
-matcher = Coma(use_instances=True, java_xmx="1024m")
+matcher = Coma(use_instances=True, java_xmx="2048m")
 matches = dict()
+comparisonNumber = 0
 for i in range(len(schemaList)-1):
     for j in range(i+1, len(schemaList)):
-        print(f"\rcomparison number: {i*j + j}", end="")
+        # if (i*j + j)!=15: continue
+        comparisonNumber += 1
+        print(f"\rcomparison number: {comparisonNumber}", end="")
         schemaName1 = schemaNames[i]
         schemaName2 = schemaNames[j]
         result = valentine_match(schemaList[i], schemaList[j], matcher, schemaNames[i], schemaNames[j])
         for key in result:
             matches[key] = result
+print()
 with open("matches", "w") as matchesJson:
     json.dump(matches, matchesJson)
