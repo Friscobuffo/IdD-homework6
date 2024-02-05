6ab4f9394491e704a3f8097b0fbab2db0d3e7879
ricciric <riki.marasca@gmail.com>
2024-02-05 12:38:15 +0100

    Aggiunta parallelizzazione al blocking-country

--- blocking/blocking-country.py
+++ blocking/blocking-country.py
@@ -1,34 +1,56 @@
 import country_converter as coco
 import json
 import os
+from multiprocessing import Pool, Manager
 
 
-def block_creator(entry, blocks, counter):
+def block_creator(args):
+    entry,blocks, blocks_lock = args
+    country = coco.convert(entry["Headquarter"], to='name', enforce_list=False) # Da modificare in "country"
     
-    country = coco.convert(entry["Headquarter"], to='name', enforce_list=False)
     if type(country) == list:
-        counter += 1
+        return 1
     else:
-        if country in blocks:
-            blocks[country].append(entry)
-        else:
-            blocks[country] = [entry]
+        with blocks_lock:
+            if country in blocks:
+                blocks[country].append(entry)
+            else:
+                blocks[country] = [entry]
+        return 0    
 
-    return counter
+def process_chunk(args):
+    chunk, blocks, blocks_lock = args
+    result = 0
+    for entry in chunk:
+        result += block_creator((entry, blocks, blocks_lock))
+    return result
 
+def main():
+    with Pool(processes=num_processes) as pool:
+        json_file_path = os.path.join('sources-json', 'MarScoToc-ambitionbox.com.json')
 
-blocks = {}
-counter = 0
-json_file_path = os.path.join('sources-json', 'MarScoToc-ambitionbox.com.json') # Prova
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
 
-with open(json_file_path, 'r') as json_file:
-        data = json.load(json_file)
-        for entry in data:
-            counter = block_creator(entry, blocks, counter)
-print(blocks)
+        chunk_size = len(data) // num_processes
+        chunks = [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
 
+        manager = Manager()
+        blocks = manager.dict()
+        blocks_lock = manager.Lock()
+
+        results = pool.map(process_chunk, [(chunk, blocks, blocks_lock) for chunk in chunks])
     
-    
+    counter = sum(results)
+    print(f"Blocks dictionary: {len(blocks)}")
+    print(f"Errori nell blocking del country: {counter}")
         
+if __name__ == "__main__":
+    num_processes = 8
+    main()
 
-    
+        
+        
+            
+
+        
