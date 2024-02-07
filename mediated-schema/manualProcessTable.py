e026e97d30d043b0ee90781242e2e55cf0ca5123
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-02-07 17:50:49 +0100

    preprocessing

---
+++ mediated-schema/manualProcessTable.py
@@ -0,0 +1,129 @@
+import json
+import os
+import country_converter as coco
+from multiprocessing import Process
+import multiprocessing
+from time import time
+import shutil
+
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+FINAL_TABLE_PATH = ABS_PATH + "/final-table.json"
+OUTPUT_DIRECTORY = ABS_PATH + "/processed-chunks"
+
+def readJsonFile(filePath):
+    with open(filePath, 'r') as jsonfile:
+        data = json.load(jsonfile)
+    return data
+
+def saveJson(element, path):
+    with open(path, "w") as jsonFile:
+        json.dump(element, jsonFile, indent=4)
+
+def removeNonNumerical(text):
+    return ''.join(char for char in text if char.isdigit())
+
+def removeAlpha(text):
+    return ''.join(char for char in text if not char.isalpha())
+
+def parseChunk(chunk, processNumber):
+    # processing founding_year
+    for entry in chunk:
+        foundingYear = entry["founding_year"]
+        foundingDate = entry["founding_date"]
+        if (foundingYear == "" or foundingYear == "Not found"):
+            if foundingDate != "":
+                year = removeNonNumerical(foundingDate.split("/")[2])
+                if len(year) == 4:
+                    entry["founding_year"] = year
+                    continue
+            entry["founding_year"] = ""
+            continue
+        if len(foundingYear) == 4:
+            entry["founding_year"] =  foundingYear
+            continue
+        onlyNumbers = removeAlpha(foundingYear)
+        onlyNumbersList = onlyNumbers.split()
+        actualFoundingYear = None
+        for element in onlyNumbersList:
+            element = removeNonNumerical(element)
+            if len(element) == 4:
+                actualFoundingYear = element
+                break
+        if actualFoundingYear:
+            entry["founding_year"] =  actualFoundingYear
+        else:
+            entry["founding_year"] = ""
+
+    # processing country
+    i=0
+    for entry in chunk:
+        i+=1
+        if i%15==0:
+            print("process "+str(processNumber)+": "+str(i/len(chunk)*100)+"%")
+        country = entry["country"]
+        components = country.split(",")
+        if country.strip() == "":
+            city = entry["location_city"]
+            if not city: continue
+            maybeCountry = entry["location_city"].split(",")[-1].strip()
+            if not maybeCountry: continue
+            processedCountry = coco.convert(maybeCountry, to='name')
+            if processedCountry != "not found":
+                entry["country"] = processedCountry
+            continue
+        if len(components)>1:
+            last = components[-1].strip()
+            processedCountry = coco.convert(last, to='name')
+        else:
+            processedCountry = coco.convert(components[0], to='name')
+        if type(processedCountry) == list:
+            processedCountry = processedCountry[-1] # maybe think better this thing
+        if processedCountry == "not found":
+            entry["country"] = ""
+        else:
+            entry["country"] = processedCountry
+        if not entry["location_city"]:
+            entry["location_city"] = country
+
+    # saving json
+    outputPath = OUTPUT_DIRECTORY + "/chunk" + str(processNumber) + ".json"
+    saveJson(chunk, outputPath)
+    print(f"finished {processNumber} process")
+
+if __name__ == "__main__":
+    if os.path.exists(OUTPUT_DIRECTORY):
+        shutil.rmtree(OUTPUT_DIRECTORY)
+    os.mkdir(OUTPUT_DIRECTORY)
+    finalTable = readJsonFile(FINAL_TABLE_PATH)
+    start = time()
+    numProcs = multiprocessing.cpu_count()
+    procs = []
+    totalEntries = len(finalTable)
+    chunk_size = totalEntries // numProcs
+    remainder = totalEntries - chunk_size*numProcs
+    chunks = [finalTable[i : i + chunk_size] for i in range(0, totalEntries-remainder, chunk_size)]
+    if remainder != 0:
+        for i, elem in enumerate(finalTable[-remainder:]):
+            chunks[i].append(elem)
+    try:
+        for i in range(numProcs):
+            proc = Process(target=parseChunk, args=(chunks[i], i))
+            procs.append(proc)
+            proc.start()
+        for proc in procs:
+            proc.join()
+        totalTime = str(time() - start)
+        print(f"total time: {totalTime}")
+        files = os.listdir(OUTPUT_DIRECTORY)
+        finalProcessedTable = []
+        for file in files:
+            finalProcessedTable += readJsonFile(OUTPUT_DIRECTORY + "/" + file)
+        finalProcessedTablePath = ABS_PATH + "/processed-final-table.json"
+        saveJson(finalProcessedTable, finalProcessedTablePath)
+        # if os.path.exists(OUTPUT_DIRECTORY):
+        #     shutil.rmtree(OUTPUT_DIRECTORY)
+    except KeyboardInterrupt:
+        print("\nclosing all processes")
+        for proc in procs:
+            proc.terminate()
+        print("closed all processes")
---
+++ mediated-schema/manualProcessTable2.py
@@ -0,0 +1,98 @@
+import os
+import json
+from geopy.geocoders import Nominatim
+import country_converter as coco
+import translators as ts
+import re
+
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+PROCESSED_FINAL_TABLE_PATH = ABS_PATH + "/processed-final-table.json"
+
+with open(PROCESSED_FINAL_TABLE_PATH, 'r') as jsonfile:
+    processedFinalTable = json.load(jsonfile)
+
+total = 0
+for entry in processedFinalTable:
+    country = entry["country"]
+    city = entry["location_city"]
+    if not country and city:
+        total += 1
+geolocator = Nominatim(user_agent="frisco")
+i = 0
+
+countryLabels = dict()
+def processCountryCoco(country, labels):
+    if country in labels:
+        return labels[country]
+    processedCountry = coco.convert(country, to='name')
+    if processedCountry != "not found":
+        labels[country] = processedCountry
+        return processedCountry
+    label = ts.translate_text(country, translator="bing")
+    label = coco.convert(label, to="name")
+    labels[country] = label
+    return label
+
+def geolocate(city, errors=0):
+    try:
+        location = geolocator.geocode(city)
+    except Exception:
+        if errors == 20:
+            print("too many errors, saving now before exiting")
+            PROCESSED_FINAL_TABLE_PATH2 = ABS_PATH + "/processed-final-table2.json"
+            with open(PROCESSED_FINAL_TABLE_PATH2, "w") as jsonFile:
+                json.dump(processedFinalTable, jsonFile, indent=4)
+            raise Exception
+        location = geolocate(city, errors+1)
+    return location
+
+cache = dict()
+cacheHits = 0
+errors = 0
+print(total)
+allCountriesOccurrences = dict()
+for entry in processedFinalTable:
+    country = entry["country"]
+    city = entry["location_city"]
+    if (not country) and city:
+        i += 1
+        print(f"i: {str(i)} cache size: {len(cache)} cache hits: {cacheHits} errors: {errors}")
+        if re.match(r'^IT\d{11}$', city):
+            country = processCountryCoco("Italy", countryLabels)
+            entry["country"] = country
+            continue
+        if city in cache:
+            cacheHits += 1
+            country = cache[city]
+            if country != "not found":
+                entry["country"] = country
+                if country not in allCountriesOccurrences:
+                    allCountriesOccurrences[country] = 0
+                allCountriesOccurrences[country] += 1
+            continue
+        location = geolocate(city)
+        if location == None:
+            last = city.split(",")[-1]
+            location = geolocate(last)
+            if location == None:
+                print("\nlocation still None :(")
+                print(city)
+                continue
+        country = location.address.split(",")[-1].strip()
+        processedCountry = processCountryCoco(country, countryLabels)
+        if processedCountry and (processedCountry != "not found"):
+            entry["country"] = processedCountry
+            if processedCountry not in allCountriesOccurrences:
+                allCountriesOccurrences[processedCountry] = 0
+            allCountriesOccurrences[processedCountry] += 1
+        else:
+            print(f"not found this {city}")
+            errors += 1
+        cache[city] = processedCountry
+
+PROCESSED_FINAL_TABLE_PATH2 = ABS_PATH + "/processed-final-table2.json"
+with open(PROCESSED_FINAL_TABLE_PATH2, "w") as jsonFile:
+    json.dump(processedFinalTable, jsonFile, indent=4)
+
+for country in allCountriesOccurrences:
+    print(f"{country} occurrences: {allCountriesOccurrences[country]}")
