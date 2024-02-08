65fd70c7f683d8784a726b37e533bc905af1b513
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-02-08 12:45:28 +0100

    funziona pairwise match in c

--- proveC/pairwiseMatching.c
+++ proveC/pairwiseMatching.c
@@ -3,6 +3,10 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include "cJSON.h"
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 // to compile run:
 // gcc -o prova pairwiseMatching.c cJSON.c
@@ -19,6 +23,13 @@ char* removeNonAlphanumeric(char *str) {
     }
     *dst = '\0';
     return result;
+}
+
+void toLowercase(char *str) {
+    while (*str) {
+        *str = tolower(*str);
+        str++;
+    }
 }
 
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
@@ -51,23 +62,33 @@ void mergeSecondJsonIntoFirst(cJSON* first, cJSON* second) {
     cJSON* current = first->child;
     while (current != NULL) {
         char* attribute_name = current->string;
-        cJSON* attribute = cJSON_GetObjectItemCaseSensitive(first, attribute_name);
-        if (attribute->valuestring == "") {
-            attribute->valuestring = cJSON_GetObjectItemCaseSensitive(second, attribute_name)->valuestring;
-        }
         current = current->next;
+        cJSON* attribute = cJSON_GetObjectItemCaseSensitive(first, attribute_name);
+        if (attribute->valuestring == NULL) {
+            attribute->valuestring = (char*)malloc(sizeof(char));
+            attribute->valuestring[0] = '\0';
+        }
+        if (strcmp(attribute->valuestring, "") == 0) {
+            char* value = cJSON_GetObjectItemCaseSensitive(second, attribute_name)->valuestring;
+            if (value == NULL) continue;
+            char* copy = (char*)malloc((strlen(value)+1) * sizeof(char));
+            strcpy(copy, value);
+            free(attribute->valuestring);
+            attribute->valuestring = copy;
+        }
     }
 }
 
-int main() {
+void computePairwiseMatchingOnJson(char* jsonFilePath, char* outputFilePath, int* totalElementsRemoved) {
+    printf("computing: %s\n", jsonFilePath);
     // reading json file
     FILE* file;
     char* json_string;
     long file_size;
-    file = fopen("jsonFile.json", "rb");
+    file = fopen(jsonFilePath, "rb");
     if (file == NULL) {
         perror("Error opening file");
-        return 1;
+        exit(1);
     }
     fseek(file, 0, SEEK_END);
     file_size = ftell(file);
@@ -75,7 +96,7 @@ int main() {
     json_string = (char*)malloc(file_size * sizeof(char));
     if (json_string == NULL) {
         perror("Memory allocation error");
-        return 1;
+        exit(1);
     }
     fread(json_string, sizeof(char), file_size, file);
     fclose(file);
@@ -84,7 +105,7 @@ int main() {
     cJSON* json_array = cJSON_Parse(json_string);
     if (json_array == NULL) {
         printf("Error parsing JSON: %s\n", cJSON_GetErrorPtr());
-        return 1;
+        exit(1);
     }
     free(json_string);
 
@@ -105,6 +126,7 @@ int main() {
         cJSON* name_outer = cJSON_GetObjectItemCaseSensitive(json_item_outer, "company_name");
         char* company_name_outer = name_outer->valuestring;
         char* company_name_outer_cleaned = removeNonAlphanumeric(company_name_outer);
+        toLowercase(company_name_outer_cleaned);
         int j = i+1;
         for (json_item_inner = json_item_outer->next; json_item_inner != NULL; json_item_inner = json_item_inner->next, j++) {
             if (removed[j])
@@ -113,10 +135,11 @@ int main() {
             cJSON* name_inner = cJSON_GetObjectItemCaseSensitive(json_item_inner, "company_name");
             char* company_name_inner = name_inner->valuestring;
             char* company_name_inner_cleaned = removeNonAlphanumeric(company_name_inner);
+            toLowercase(company_name_inner_cleaned);
             float distance = levenshteinDistance(company_name_outer_cleaned, company_name_inner_cleaned);
             if (distance < 0.13) {
                 removed[j] = 1;
-                mergeSecondJsonIntoFirst(json_item_inner, json_item_outer);
+                mergeSecondJsonIntoFirst(json_item_outer, json_item_inner);
                 matchesFound++;
             }
             free(company_name_inner_cleaned);
@@ -128,10 +151,11 @@ int main() {
     printf("\n");
 
     // creating copy without duplicates
+    *totalElementsRemoved = *totalElementsRemoved + matchesFound;
     cJSON* output_array = cJSON_CreateArray();
     if (output_array == NULL) {
         printf("Error creating cJSON array.\n");
-        return 1;
+        exit(1);
     }
     i = 0;
     cJSON_ArrayForEach(json_item_outer, json_array) {
@@ -154,12 +178,12 @@ int main() {
     json_string = cJSON_Print(output_array);
     if (json_string == NULL) {
         printf("Error converting cJSON object to JSON string.\n");
-        return 1;
+        exit(1);
     }
-    file = fopen("output.json", "w");
+    file = fopen(outputFilePath, "w");
     if (file == NULL) {
         printf("Error opening file for writing.\n");
-        return 1;
+        exit(1);
     }
     fputs(json_string, file);
     fclose(file);
@@ -168,5 +192,48 @@ int main() {
     // freeing heap
     cJSON_Delete(json_array);
     cJSON_Delete(output_array);
+}
+
+#define BLOCKS_DIRECTORY "custom-blocks"
+#define OUTPUT_DIRECTORY "output"
+
+int main() {
+    char command[100];
+    sprintf(command, "rm -r %s", OUTPUT_DIRECTORY);
+    if (system(command) != 0) {
+        perror("Error removing directory");
+        return 1;
+    }
+    mkdir(OUTPUT_DIRECTORY, 0777);
+
+    DIR *directory;
+    struct dirent *entry;
+    directory = opendir(BLOCKS_DIRECTORY);
+    if (directory == NULL) {
+        perror("Unable to open directory");
+        return 1;
+    }
+    char path[200];
+    char outputPath[200];
+    int totalElementsRemoved = 0;
+    while ((entry = readdir(directory)) != NULL) {
+        char* jsonName = entry->d_name;
+        if (strcmp(jsonName,  ".json") == 0)
+            continue;
+        if (strlen(jsonName) > 2) {
+            path[0] = '\0';
+            strcat(path, BLOCKS_DIRECTORY);
+            strcat(path, "/");
+            strcat(path, jsonName);
+
+            outputPath[0] = '\0';
+            strcat(outputPath, OUTPUT_DIRECTORY);
+            strcat(outputPath, "/");
+            strcat(outputPath, jsonName);
+            computePairwiseMatchingOnJson(path, outputPath, &totalElementsRemoved);
+        }
+    }
+    printf("total elements removed: %d\n", totalElementsRemoved);
+    closedir(directory);
     return 0;
 }
