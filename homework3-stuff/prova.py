bd34ab4761a93cda8162204ff5203e35edad693c
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-02-11 18:10:57 +0100

    finish punto 4

--- homework3-stuff/prova.py
+++ homework3-stuff/prova.py
@@ -3,6 +3,9 @@ import os
 import pandas as pd
 from valentine import valentine_match
 from valentine.algorithms import Coma
+import pickle
+import re
+import shutil
 
 def readJsonFile(filePath):
     with open(filePath, 'r') as jsonfile:
@@ -28,104 +31,112 @@ for fileName in filesNames:
         else:
             row = cell["Coordinates"]["row"]-1
             column = cell["Coordinates"]["column"]
-            table[row][column] = cell["cleanedText"]   
+            text = cell["cleanedText"]
+            pattern = r"\d{2}x\d{2}px"
+            text = re.sub(pattern, '', text)
+            text = text.strip()
+            table[row][column] = text  
     tableDataframe = pd.DataFrame(table, columns=headers)
     tables[tableName] = tableDataframe
 
-
-
-
 PROCESSED_FINAL_TABLE_PATH = ABS_PATH + "/processed-final-table2.json"
 finalTable = pd.read_json(PROCESSED_FINAL_TABLE_PATH)
-finalTable = finalTable.iloc[:100]
+finalTable = finalTable.iloc[:200]
+oldMediatedSchemaHeader = set(finalTable.columns.tolist())
+newMediatedSchemaHeader = set(finalTable.columns.tolist())
+finalTable.rename(columns={'country': 'country_headquarters'}, inplace=True)
 
+MATCHES_PATH = ABS_PATH + "/matches"
+if os.path.exists(MATCHES_PATH):
+    with open(MATCHES_PATH, 'rb') as pickleFile:
+        matches = pickle.load(pickleFile)
+else:
+    matcher = Coma(use_instances=True, java_xmx="2048m")
+    matches = dict()
+    for i, tableName in enumerate(tables):
+        table = tables[tableName]
+        print(f"{i} - {tableName}")
+        result = valentine_match(finalTable, table.iloc[:200], matcher)
+        for key in result:
+            score = result[key]
+            key = (("final-table", key[0][1]), (tableName, key[1][1]))
+            matches[key] = score    
+    with open(MATCHES_PATH, "wb") as file:
+        pickle.dump(matches, file)
 
-matcher = Coma(use_instances=True, java_xmx="2048m")
-matches = dict()
-for i, tableName in enumerate(tables):
-    table = tables[tableName]
-    print(f"{i} - {tableName}")
-    result = valentine_match(finalTable, table.iloc[:100], matcher)
-    for key in result:
-        score = result[key]
-        key = (("final-table", key[0][1]), (tableName, key[1][1]))
-        matches[key] = score
-
+candidateTables = set()
 for match in matches:
-    # print(match)
     score = matches[match]
     if match[0][1]=="company_name" and score>0.3:
-        print(match, score)
+        tableName = match[1][0]
+        candidateTables.add(tableName)
+print(f"number of candidate tables: {len(candidateTables)}")
 
-quit()
+processedMatches = dict()
+for match in matches:
+    tableName = match[1][0]
+    if tableName not in candidateTables: continue
+    score = matches[match]
+    if score < 0.3: continue
+    originalColumn = match[0][1]
+    toColumn = match[1][1]
+    if tableName not in processedMatches:
+        processedMatches[tableName] = [(originalColumn, toColumn, score)]
+    else:
+        processedMatches[tableName].append((originalColumn, toColumn, score))
 
-sets = []
-for key in matches:
-    dataset0 = key[0]
-    dataset1 = key[1]
-    combined = set()
-    combined.add(dataset0)
-    combined.add(dataset1)
-    j = 0
-    for s in sets:
-        if dataset0 in s:
-            temp = sets.pop(j)
-            combined = combined.union(temp)
-        j += 1
-    j = 0
-    for s in sets:
-        if dataset0 in s:
-            temp = sets.pop(j)
-            combined = combined.union(temp)
-        j += 1
-    sets.append(combined)
-
-PROCESSED_MATCHES_DIRECTORY = ABS_PATH + "/processed-matches"
-if os.path.exists(PROCESSED_MATCHES_DIRECTORY):
-    shutil.rmtree(PROCESSED_MATCHES_DIRECTORY)
-os.mkdir(PROCESSED_MATCHES_DIRECTORY)
-for i, s in enumerate(sets):
-    fileName = "matches"+str(i)+".txt"
-    filePath = PROCESSED_MATCHES_DIRECTORY + "/" + fileName
-    with open(filePath, 'w') as f:
-        for elem in s:
-            f.write(str(elem) + '\n')
-
-labelsMediatedSchema = []
-for s in sets:
-    print(s)
-    print("what name do you want to give to this set of attibutes?")
-    name = input()
+for tableName in processedMatches:
+    print(tableName)
+    print(processedMatches[tableName])
     print()
-    labelsMediatedSchema.append(name)
 
-SOURCES_DIRECTORY = ABS_PATH + "/sources-json"
-files = os.listdir(SOURCES_DIRECTORY)
-schemas = []
-for file in files:
-    filePath = SOURCES_DIRECTORY + "/" + file
-    with open(filePath, 'r') as jsonfile:
-        data = json.load(jsonfile)[0]
-    for key in data:
-        label = None
-        for i, s in enumerate(sets):
-            for elem in s:
-                dataset = elem[0]
-                column = elem[1]
-                if dataset != file: continue
-                if key != column: continue
-                label = labelsMediatedSchema[i]
-                break
-            if label: break
-        if not label:
-            print(f"{key} from {file} needs a label, input wanted label:")
-            label = input()
-        data[key] = label
-    data["dataset"] = file
-    schemas.append(data)
+headerFinalTable = finalTable.columns.tolist()
+print()
 
-mediatedSchemaPath = ABS_PATH + "/mediated-schema.json"
-if os.path.exists(mediatedSchemaPath):
-    os.remove(mediatedSchemaPath)
-with open(mediatedSchemaPath, "w") as json_file:
-    json.dump(schemas, json_file, indent=4)
+OUTPUT_PATH = ABS_PATH + "/output"
+if os.path.exists(OUTPUT_PATH):
+    shutil.rmtree(OUTPUT_PATH)
+os.mkdir(OUTPUT_PATH)
+
+print(len(processedMatches))
+
+totalNewEntries = 0
+for number,tableName in enumerate(processedMatches):
+    table = tables[tableName]
+    oldHeader = table.columns.tolist()
+    print(tableName)
+    print("old header", end = "")
+    print(oldHeader)
+    newHeader = table.columns.tolist()
+    for i, columnName in enumerate(oldHeader):
+        for originalColumn, toColumn, _ in processedMatches[tableName]:
+            if toColumn == columnName:
+                newHeader[i] = originalColumn
+    if "country_headquarters" in newHeader:
+        position = newHeader.index("country_headquarters")
+        newHeader[position] = "country"
+    print("new header", end = "")
+    print(newHeader)
+    print("\ngood matches:")
+    for columnName in oldHeader:
+        for match in matches:
+            score = matches[match]
+            if match[1] == (tableName, columnName) and score>0.3 and match[0][1] != "company_name":
+                print(match[0][1], " - ", match[1][1], score)
+    table.columns = newHeader
+    print()
+    newMediatedSchemaHeader.update(newHeader)
+    print(table.iloc[:5])
+    tablePath = OUTPUT_PATH+"/"+str(number)+'.json'
+    print(tablePath)
+    table.to_json(tablePath, orient='records', indent=4)
+    print("\n\n\n")
+    totalNewEntries += len(table)
+
+print(f"total new entries: {totalNewEntries}")
+print()
+print(f"old mediated schema: {len(oldMediatedSchemaHeader)}")
+print(oldMediatedSchemaHeader)
+print()
+print(f"new mediated schema: {len(newMediatedSchemaHeader)}")
+print(newMediatedSchemaHeader)
