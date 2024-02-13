5ff196cad18502dee61aa6c4673f47bb544dbabe
ricciric <riki.marasca@gmail.com>
2024-02-13 12:54:33 +0100

    Calcolato numero medio confronti sulle due strategie di blocking

--- graphic_creator.py
+++ graphic_creator.py
@@ -6,8 +6,8 @@ def file_histogram(directory):
     # Get the list of files in the directory
     files = os.listdir(directory)
     # Create dictionaries to store file names and entry counts
-    file_entries = {}
-
+    sum_files = 0
+    tot_comparisons = 0
     # Iterate over each file
     for file in files:
         # Read the JSON file into a DataFrame
@@ -17,47 +17,16 @@ def file_histogram(directory):
             print(f"Error reading file: {file}. Skipping...")
             continue
 
-        # Count the number of entries for the current file
-        num_entries = len(data)
+        tot_comparisons += (len(data) * (len(data) - 1))/2
+
+        sum_files+=1
+    
         
-        file_name = os.path.splitext(file)[0]
-        # Store the file name and entry count
-        file_entries[file_name] = num_entries
+    print(tot_comparisons/sum_files) # numero medio di confronti
+        
 
-    # Sort files based on the number of entries
-    sorted_files = sorted(file_entries.items(), key=lambda x: x[1], reverse=True)
-
-    # Create lists to store file names and entry counts
-    file_names = []
-    entry_counts = []
-
-    # Iterate over sorted files
-    for file, num_entries in sorted_files:
-        file_names.append(file)
-        entry_counts.append(num_entries)
-
-    # Plot the histogram
-    plt.figure(facecolor='#37474f')  # Set figure background color
-    plt.bar(file_names, entry_counts, color='darkcyan')  # Bar color
-    plt.xlabel('File Names', color='white')  # X-axis label color
-    plt.ylabel('Number of Entries', color='white')  # Y-axis label color
-    plt.title('Entries for country', color='white')  # Title color
-    plt.xticks(color='white')  # X-axis tick labels color
-    plt.yticks(color='white')  # Y-axis tick labels color
-    plt.grid(axis='y', linestyle='--', color='gray')  # Grid color
-
-    # Calculate average number of entries
-    average_entries = sum(entry_counts) // len(entry_counts)
-
-    # Plot average line
-    plt.axhline(y=average_entries, color='red', linestyle='--', linewidth=0.9, label=f'Average: ~{average_entries:.2f}')
-    plt.legend()
-
-    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
-    plt.tight_layout()  # Adjust layout to prevent overlap of labels
-    plt.show()
 
 # Call the function with the directory path
 ABS_PATH = os.path.dirname(os.path.abspath(__file__))
-TABLE_PATH = os.path.join(ABS_PATH + '/sources-json')
+TABLE_PATH = os.path.join(ABS_PATH + '/pairwise-matching/output')
 file_histogram(TABLE_PATH)
--- pairwise-matching/makeBlocks.py
+++ pairwise-matching/makeBlocks.py
@@ -2,7 +2,7 @@ import os
 import json
 import shutil
 
-def makeBlocks(keys, char_num, custom_chars):
+def makeBlocks_company(keys, char_num, custom_chars):
     absPath = os.path.dirname(os.path.abspath(__file__))
     
     finaTablePath = absPath + "/../mediated-schema/processed-final-table2.json"
@@ -35,7 +35,37 @@ def makeBlocks(keys, char_num, custom_chars):
             blockPath = absPath + "/custom-blocks/%s.json" %key
         with open(blockPath, "w") as json_file:
             json.dump(blocks[key], json_file, indent=4)
+    
+def makeBlocks_country(keys, char_num):
+    absPath = os.path.dirname(os.path.abspath(__file__))
+    
+    finaTablePath = absPath + "/../mediated-schema/processed-final-table2.json"
+    with open(finaTablePath, 'r') as jsonfile:
+        finalTable = json.load(jsonfile)
+
+    blocks = dict()
+    for key in keys:
+        for row in finalTable:
+            new_key = row[key]
+
+            if new_key not in blocks:
+                blocks[new_key] = []
+            blocks[new_key].append(row)
+    
+    print(len(blocks))
+
+    
+    if os.path.exists(absPath + "/custom-blocks"):
+        shutil.rmtree(absPath + "/custom-blocks")
+    os.mkdir(absPath + "/custom-blocks")
+
+    for key in blocks.keys():
+        blockPath = absPath + "/custom-blocks/%s.json" %key
+        if os.path.exists(blockPath):
+            os.remove(blockPath)
+        with open(blockPath, "w") as json_file:
+            json.dump(blocks[key], json_file, indent=4)
 
 
 if __name__ == "__main__":
-    makeBlocks(["company_name"], 2, True)
+    makeBlocks_company(["company_name"], 2, True)
